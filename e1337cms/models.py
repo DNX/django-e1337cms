8f1853827d291ff763171bd2d3dd2e23e82a8f41
Darii Denis <denis.darii@gmail.com>
2012-05-29 15:43:23 +0200

    added some fields to Page model

--- e1337cms/models.py
+++ e1337cms/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(_('title'), max_length=250)
+    slug = models.SlugField(_('slug'), max_length=200)
+    published = models.BooleanField(_("is published"), blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True,
+                                related_name='children', db_index=True,
+                                verbose_name=_('parent'))
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    changed_date = models.DateTimeField(auto_now=True)
