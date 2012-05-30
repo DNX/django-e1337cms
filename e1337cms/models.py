ac64e19a15394aa2af9d19bcdf28b133444a4f64
Darii Denis <denis.darii@gmail.com>
2012-05-30 12:40:54 +0200

    added content field in Page model

--- e1337cms/models.py
+++ e1337cms/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Page(models.Model):
     title = models.CharField(_('title'), max_length=250)
     slug = models.SlugField(_('slug'), max_length=200)
+    content = models.TextField(_('content'))
     published = models.BooleanField(_("is published"), blank=True)
     parent = models.ForeignKey('self', null=True, blank=True,
                                 related_name='children', db_index=True,
@@ -12,3 +13,8 @@ class Page(models.Model):
 
     creation_date = models.DateTimeField(auto_now_add=True)
     changed_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['creation_date']
+        verbose_name = _('page')
+        verbose_name_plural = _('pages')
