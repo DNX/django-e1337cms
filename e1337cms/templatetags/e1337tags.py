83373a5ce5c4e2c63ce2e03fc74e62494562aee7
Darii Denis <denis.darii@gmail.com>
2012-05-31 12:14:42 +0200

    added rst block parser and some tests

--- e1337cms/templatetags/e1337tags.py
+++ e1337cms/templatetags/e1337tags.py
@@ -1,17 +1,45 @@
+from django.conf import settings
+from django import template
+from django.utils.encoding import smart_str, force_unicode
+from django.utils.safestring import mark_safe
 from django.template.base import Node
 from django.template.base import Library
 
 register = Library()
 
+
 class RstNode(Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
     def render(self, context):
-        # TODO: convert this content to html
-        return 'output html'
+        rst_content = self.nodelist.render(context)
+        try:
+            from docutils.core import publish_parts
+        except ImportError:
+            if settings.DEBUG:
+                raise template.TemplateSyntaxError("Error in 'rst' parsing: The Python docutils library isn't installed.")
+            return force_unicode(rst_content)
+        else:
+            docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {})
+            parts = publish_parts(source=smart_str(rst_content), writer_name="html4css1", settings_overrides=docutils_settings)
+            return mark_safe(force_unicode(parts["html_body"]))
+
 
 @register.tag
 def rst(parser, token):
     """
     rst to html everything between ``{% rst %}`` and ``{% endrst %}``.
+
+    Example usage::
+
+        {% rst %}
+            Important title
+            ===============
+
+            content...
+        {% endrst %}
     """
-    parser.skip_past('endrst')
-    return RstNode()
+    nodelist = parser.parse(('endrst',))
+    parser.delete_first_token()
+    return RstNode(nodelist)
--- e1337cms/tests.py
+++ e1337cms/tests.py
@@ -6,17 +6,39 @@ from e1337cms.models import Page
 
 
 class ViewTest(TestCase):
-    def test_simple_page_render(self):
-        r = self.client.get(reverse("page", kwargs={'slug': 'dummy-slug'}))
-        self.assertEquals(r.status_code, 404)
-        page = Page.objects.create(title='First page title',
+
+    def setUp(self):
+        self.page = Page.objects.create(title='First page title',
             slug='first-page-slug',
             content='first page content')
+
+    def test_page_dont_exists(self):
+        r = self.client.get(reverse("page", kwargs={'slug': 'dummy-slug'}))
+        self.assertEquals(r.status_code, 404)
+
+    def test_simple_page_render(self):
         r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
         self.assertContains(r, "first page content")
         self.assertNotContains(r, "First page title")
-        page.content = '{{ page.title }}\nfirst page content'
-        page.save()
+        self.page.content = '{{ page.title }}\nfirst page content'
+        self.page.save()
         r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
         self.assertContains(r, "first page content")
         self.assertContains(r, "First page title")
+
+    def test_rst_block(self):
+        # title
+        self.page.content = '{% load e1337tags %}{% rst %}Page title\n=================={% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<h1 class=\"title\">Page title</h1>")
+        # bold text
+        self.page.content = '{% load e1337tags %}{% rst %}**bold text**{% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<div class=\"document\">\n<p><strong>bold text</strong></p>\n</div>\n")
+        # italic text
+        self.page.content = '{% load e1337tags %}{% rst %}*italic text*{% endrst %}'
+        self.page.save()
+        r = self.client.get(reverse("page", kwargs={'slug': 'first-page-slug'}))
+        self.assertContains(r, "<div class=\"document\">\n<p><em>italic text</em></p>\n</div>\n")
