0308f5830d925234677142781e727ce14501d40f
Darii Denis <denis.darii@gmail.com>
2012-06-01 13:33:56 +0200

    fixed a bug in e1337cms urlpatterns

--- e1337cms/urls.py
+++ e1337cms/urls.py
@@ -4,5 +4,5 @@ from e1337cms import settings as e1337settings
 
 urlpatterns = patterns('e1337cms.views',
     url(r'^(?P<slug>[0-9A-Za-z-_.//]+)/$', PageDetail.as_view(), name='page'),
-    url(r'$', PageDetail.as_view(), name='index_page', kwargs={'slug': e1337settings.INDEX_PAGE_SLUG, })
+    url(r'^$', PageDetail.as_view(), name='index_page', kwargs={'slug': e1337settings.INDEX_PAGE_SLUG, })
     )
